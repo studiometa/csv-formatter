f6eda2d01313a2110571fbfbc52ad8ef78cc1096
Titouan <titouan@studiometa.fr>
2016-06-27 18:04:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const csv = require('fast-csv');
+let array = [];
+
+const file = process.argv[2] ? process.argv[2] : `${__dirname}/annuaire_vignoble_31-05-2016.csv`;
+
+// Parse the original CSV file
+csv
+	.fromPath(file, {
+		headers: true,
+		delimiter: ';'
+	})
+	.on('data', (data) => {
+		// Push each row to the array
+		array.push(data);
+	})
+	.on('end', () => {
+
+		// Write new file with correct
+		// CSV formatting
+
+		const options = {
+			headers: true,
+			delimiter: ';',
+			quote: '"',
+			quoteHeaders: true,
+			quoteColumns: true
+		};
+
+		csv.writeToString(array, options, (err, data) => {
+			if (err) throw err;
+			console.log(data);
+		})
+	});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# CSV Formatter
+
+A little script to format CSV files with `;` as delimiters and `"` as data containers.
+
+Install it via NPM:
+
+```
+npm i csv-formatter
+```
+
+And use it with the following command:
+
+```
+csv-formatter /path/to/file-to-format.csv > /path/to/formatted-file.csv
+```
